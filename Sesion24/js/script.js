533b390b1228bd655978202f441fed8344f1765e
Flavio <flavio28_95@hotmail.com>
2024-08-05 18:42:03 -0500

    agregando archivos

---
+++ Sesion24/js/script.js
@@ -0,0 +1,17 @@
+/*var i = 0;
+for(i;i<5;i++){
+    console.log(i)
+}
+*/
+
+/*for(var i = 1; i<10; i+=2){
+    console.log(i+1)
+}*/
+
+for(var i=1; i<21;i++){
+    console.log(i);
+}
+
+for(var i=0; i<8;i++){
+    console.log("hola")
+}
