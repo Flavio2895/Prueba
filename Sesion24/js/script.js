458bc913a580ccbe406d9e43d06d2b4358604064
Flavio <flavio28_95@hotmail.com>
2024-08-05 19:39:48 -0500

    agregando codigo al archivo script

--- Sesion24/js/script.js
+++ Sesion24/js/script.js
@@ -8,10 +8,24 @@ for(i;i<5;i++){
     console.log(i+1)
 }*/
 
-for(var i=1; i<21;i++){
+/*for(var i=1; i<21;i++){
     console.log(i);
 }
 
 for(var i=0; i<8;i++){
     console.log("hola")
+}
+*/
+var paises = ["Perú","Colombia","Alemania","Canadá"];
+var cantidad = paises.length;
+console.log(cantidad);
+
+/*console.log(paises[0]);
+console.log(paises[1]);
+console.log(paises[2]);
+console.log(paises[3]);
+*/
+
+for(var i=0; i<cantidad ; i++){
+    console.log(paises[i])
 }
